0e699198681e760eef32af232847348b019d10c1
Fabian Forsman <fabian.forsman@gmail.com>
2023-03-10 15:27:25 +0100

    Finished Part 4 apart from tests

--- Assignment 4/TestPortal.py
+++ Assignment 4/TestPortal.py
@@ -8,15 +8,29 @@ if __name__ == "__main__":
     c = PortalConnection.PortalConnection()
     
     # Write your tests here. Add/remove calls to pause() as desired. 
-    
+
+    #List info for a student.
     print("Test 1:")
-    print(c.unregister("2222222222", "CCC333"))
     print(c.getInfo("2222222222"))
     pause()
-    
+
+    # Unregister a student from an overfull course, i.e. one with more
+    # students registered than there are places on the course (you need
+    # to set this situation up in the database directly). Check that no
+    # student was moved from the queue to being registered as a result.
     print("Test 2:")
-    print(c.register("2222222222", "CCC333")); 
-    print(c.getInfo("2222222222"))
+    print(c.getInfo("5555555555"))
+    print(c.getInfo("3333333333"))
+    print(c.unregister("5555555555", "CCC222"))
+    print(c.getInfo("5555555555"))
+    print(c.getInfo("3333333333"))
+    pause()
+
+    #Register a student for an unrestricted course,
+    #and check that he/she ends up registered (print info again).
+    print("Test 3:")
+    print(c.register("6666666666", "CCC555")) #TRUE
+    print(c.getInfo("6666666666"))
     pause()
     
     
